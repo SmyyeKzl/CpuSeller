8c916d42182a1524ec78103926b9398d6dd9ff85
Casper <Casper@DESKTOP-CLAGGHP>
2021-01-19 22:01:50 +0300

    Business

---
+++ CpuSeller.Business/Concrete/CpuManager.cs
@@ -0,0 +1,44 @@
+﻿using CpuSeller.Business.Abstract;
+using CpuSeller.DataAccessLayer.Abstract;
+using CpuSeller.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CpuSeller.Business.Concrete
+{
+    public class CpuManager : ICpuService
+    {
+        private ICpuDal _cpuDal;
+        public CpuManager(ICpuDal cpuDal)
+        {
+            _cpuDal = cpuDal;
+        }
+
+        public void Add(Cpu entity)
+        {
+            _cpuDal.Add(entity);
+        }
+
+        public void Delete(Cpu entity)
+        {
+            _cpuDal.Delete(entity);
+        }
+
+        public Cpu Get(Expression<Func<Cpu, bool>> filter)
+        {
+            return _cpuDal.Get(filter);
+        }
+
+        public List<Cpu> GetAll(Expression<Func<Cpu, bool>> filter = null)
+        {
+            return _cpuDal.GetAll(filter);
+        }
+
+        public void Update(Cpu entity)
+        {
+            _cpuDal.Update(entity);
+        }
+    }
+}
