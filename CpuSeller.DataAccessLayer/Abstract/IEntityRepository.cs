997cf54f8644f8403d25159ba29d3a00c3882dcb
Casper <Casper@DESKTOP-CLAGGHP>
2021-01-19 21:41:41 +0300

    Data Access Layer

---
+++ CpuSeller.DataAccessLayer/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using CpuSeller.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CpuSeller.DataAccessLayer.Abstract
+{
+    public interface IEntityRepository<T>
+        where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ CpuSeller.DataAccessLayer/Concrete/EntityFramework/EfCpuDal.cs
@@ -0,0 +1,14 @@
+﻿using CpuSeller.DataAccessLayer.Abstract;
+using CpuSeller.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CpuSeller.DataAccessLayer.Concrete.EntityFramework
+{
+    public class EfCpuDal:
+        EfRepositoryBase<Cpu,CpuSellerContext>,
+        ICpuDal
+    {
+    }
+}
