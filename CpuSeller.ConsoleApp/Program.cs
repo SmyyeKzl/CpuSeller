a6e5686a78e0d3dceb65f21b6dd1baf3f02bed5a
Casper <Casper@DESKTOP-CLAGGHP>
2021-01-20 15:49:08 +0300

    Console.

---
+++ CpuSeller.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using CpuSeller.Business.Abstract;
+using CpuSeller.Business.CrossCuttingConcerns.DependencyResolvers.Ninject;
+using System;
+
+namespace CpuSeller.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cpuService = InstanceFactory.Get<ICpuService>();
+            var list = cpuService.GetAll();
+            foreach (var c in list)
+            {
+                Console.WriteLine(c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- CpuSeller.DataAccessLayer/Concrete/EntityFramework/CpuSellerContext.cs
+++ CpuSeller.DataAccessLayer/Concrete/EntityFramework/CpuSellerContext.cs
@@ -10,7 +10,7 @@ namespace CpuSeller.DataAccessLayer.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CpuDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
         public DbSet<Cpu> Cpus { get; set; }
     }
